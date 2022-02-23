58479653c3034320bc3b37f7de95fbf293be8a4e
WillStanks <william4800@hotmail.com>
2022-02-23 17:33:45 -0500

    Ajout : Travail 9.

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -29,42 +29,41 @@ describe('ProblemeComponent', () => {
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue("a".repeat(2));
     let errors = zone.errors || {};
-    expect(errors['minlength']).toBeTruthy();
+    expect(errors['nbreCaracteresInsuffisant']).toBe(true);
   });
 
   it("#2 | Zone PRÉNOM valide avec 3 caractères", () =>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue("a".repeat(3));
     let errors = zone.errors || {};
-    expect(errors['minlength']).toBeFalsy();
-  });
+    expect(errors['nbreCaracteresInsuffisant']).toBeUndefined();  });
 
   it("#3 | Zone PRÉNOM valide avec 200 caractères", () =>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue("a".repeat(200));
     let errors = zone.errors || {};
-    expect(errors['minlength']).toBeFalsy();
+    expect(errors['nbreCaracteresInsuffisant']).toBeUndefined();
   });
 
   it("#4 | Zone PRÉNOM invalide avec aucune valeur", () =>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue("");
     let errors = zone.errors || {};
-    expect(errors['required']).toBeTruthy();
+    expect(errors['nbreCaracteresInsuffisant']).toBe(true);
   });
 
   it("#5 | Zone PRÉNOM invalide avec 10 espaces", () =>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue(" ".repeat(10));
     let errors = zone.errors || {};
-    expect(errors['minlength']).toBeFalsy();
+    expect(errors['nbreCaracteresInsuffisant']).toBe(true);
   });
 
   it("#6 | Zone PRÉNOM invalide avec 2 espaces et 1 caractere", () =>{
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue("  1");
     let errors = zone.errors || {};
-    expect(errors['minlength']).toBeFalsy();
+    expect(errors['nbreCaracteresInsuffisant']).toBe(true);
   });
 
   
